09cad7443c1f890e9974499a446299010fe45679
taqi <taqirizvi582@gmail.com>
2022-08-28 10:24:52 +0500

    shell automation has added

---
+++ automation.ps1
@@ -0,0 +1,36 @@
+$myURI = "http://20.56.57.104:5080"
+
+
+
+$req = (Invoke-WebRequest -URI $myURI -UseBasicParsing).Content.Split([Environment]::NewLine) | sls 'alpha' | Select -Last 1
+$req =  $req | Out-String
+
+
+
+$req = $req.Split(">")[2].Split("<")[0].Trim()
+
+
+
+$myDownloadUrl = "$myURI/$req"
+
+
+
+
+$name=$req -replace ".{7}$"# 7.1.0-alpha.616.tar.gz
+
+
+
+New-Item .\$name -ItemType Directory
+cd $name
+Invoke-WebRequest $myDownloadUrl -OutFile esignature.tar.gz #C:\Users\{username}
+tar -xzvf esignature.tar.gz
+
+
+
+$req
+Start-Process ".\esignatur Desktop $name Setup.exe" -WindowStyle Hidden
+cd ..
+
+
+
+write-host "esignature app has been downloaded and exe has been executed"
