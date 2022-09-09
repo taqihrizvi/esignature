4362f32bb9242e2c0722d96e66b96793f5f01235
taqi <taqirizvi582@gmail.com>
2022-09-09 15:37:37 +0500

    Executeable file has been added

---
+++ Running.ps1
@@ -0,0 +1,25 @@
+$myURI = "http://20.56.57.104:5080"
+
+
+
+$req = (Invoke-WebRequest -URI $myURI -UseBasicParsing).Content.Split([Environment]::NewLine) | sls 'alpha' | sls -Pattern 'mac' -NotMatch | Select -Last 1
+$req =  $req | Out-String
+
+
+
+$req = $req.Split(">")[2].Split("<")[0].Trim()
+
+
+
+$myDownloadUrl = "$myURI/$req"
+
+
+
+
+$name=$req -replace ".{7}$"# 7.1.0-alpha.616.tar.gz
+
+
+$req 
+Start-Process ".\esignatur Desktop Setup $name.exe" -WindowStyle Hidden
+cd ..
+#write-host "esignature app has been downloaded and exe has been executed"
